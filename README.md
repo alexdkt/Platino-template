da349efd9101dff5d9f67db2c97cfeeea52c63e4
alexdkt <alejandrorayon@gmail.com>
2015-02-23 11:28:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Welcome to your Platino template
+
+This is a blank project.  
+
+
+
+----------------------------------
+Alejandro Rayón 2015. 
+
+twitter: @arayon_
+
+mail: arayon at arei . es
+
+web: http://www.arei.es
+
---
+++ Resources/MainScene.js
@@ -0,0 +1,52 @@
+/*
+ * MAINSCENE.JS
+ */
+
+var platino = require('co.lanica.platino');
+var ALmixer = platino.require('co.lanica.almixer');
+var chipmunk = co_lanica_chipmunk2d;
+var v = chipmunk.cpv;
+
+
+function MainScene(window, game) {
+
+	/*
+	 * DECLARING THE SCENE
+	 */
+	
+	
+	var self = platino.createScene();
+	self.addEventListener('activated', onSceneActivated);
+	self.addEventListener('deactivated', onSceneDeactivated);	
+	
+	
+
+	/*
+	 * SCENE ACTIVATED LISTENER
+	 */
+	
+    function onSceneActivated(e) {
+    	
+    	// WHEN SCENE IS ACTIVATED, STARTS THE CREATION OF ALL OBJECTS
+    	
+			
+			game.startCurrentScene();			
+			
+    };
+    
+      function onSceneDeactivated(e) {
+    	
+    	// WHEN SCENE IS DEACTIVATED, ALL INSTANCES AND TEXTURES ARE REMOVED.
+   
+
+    };
+
+
+	
+    return self;
+};
+
+
+module.exports = MainScene;
+
+
